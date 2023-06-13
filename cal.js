900fc95e3d86d803bbc694333a54db4c001cc484
Shreyas Parimi <108624499+parimi018@users.noreply.github.com>
2023-06-13 21:55:24 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,32 @@
+const output = document.querySelector(".output")
+const result = document.querySelector(".result")
+const keys = document.querySelectorAll('button')
+
+
+keys.forEach(key=>{
+    key.addEventListener("click",calculate);
+});
+function calculate()
+{
+    let buttonText = this.innerText;
+    if(buttonText==="AC"){
+        output.innerText = "";
+        result.innerText = "0";
+        return;
+    }
+
+    if(buttonText==='DEL'){
+        output.textContent = output.textContent.substring(0,output.textContent.length-1);
+        result.innerText="";
+        return;
+    }
+    if(buttonText ==='='){
+        result.innerText = eval(output.innerText);
+        output.textContent="";
+    }
+    else{
+        output.textContent+=buttonText;
+        result.innerText="";
+        return;
+    }
+}
